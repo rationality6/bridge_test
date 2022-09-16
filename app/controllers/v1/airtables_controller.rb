e619e0eef18d3720d38fedebc25e6db747efb251
Hyun Ahn <codepawn@icloud.com>
2022-09-17 01:13:04 +0900

    clean up

--- app/controllers/v1/airtables_controller.rb
+++ app/controllers/v1/airtables_controller.rb
@@ -1,18 +1,9 @@
 class V1::AirtablesController < ApplicationController
   def copy
-    puts "here"
-    puts "here"
-    puts "here"
-    puts "here"
-
     render json: {}
   end
 
   def copy_with_key
-    puts "here"
-    puts "here"
-    puts "here"
-    puts "here"
 
     render json: { key: "foo" }
   end
